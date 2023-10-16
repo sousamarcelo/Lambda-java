611717590f90fcbbb845fa7838ae5185f571256b
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-10-16 19:16:52 -0300

    Only printed emails whose salary is greater than the value stated

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import model.entities.Employee;
 
@@ -30,6 +32,20 @@ public class Program {
 				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])) );
 				line = br.readLine();
 			}
+			
+			Comparator<String> com = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			
+			System.out.print("Enter a salary: ");
+			double salaryParameter = sc.nextDouble();
+			
+			List<String> emails = list.stream()
+					.filter(p -> p.getSalary() > salaryParameter)
+					.map(p -> p.getEmail()).sorted(com)
+					.collect(Collectors.toList());
+			
+			System.out.printf("Email of people whose salary is more than %s:%n", String.format("%.2f", salaryParameter));
+			emails.forEach(System.out::println);
+					
 			
 		} catch (IOException e) {
 			System.out.println("Error :" + e.getMessage());
